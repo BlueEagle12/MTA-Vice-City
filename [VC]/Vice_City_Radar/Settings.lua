0ad64386271af85cf5c1d853c32ab13a5364598c
Blue Eagle <bluejaycjl@gmail.com>
2025-04-05 15:18:36 -0400

    Eagle Loader 3.0 Update
    Vice City Eagle Loader 3.0 Update
    
    - Fresh conversion with a brand new map converter
    - Massively improves performance
    - Adds full support for LODs
    - Stability improvements
    - Slightly updated radar
    - Object flags now fully supported
    - Many many more

---
+++ [VC]/Vice_City_Radar/Settings.lua
@@ -0,0 +1,6 @@
+radarSettings = {
+	['mapTexture'] = 'images/radar.png', -- Don't change
+	['mapTextureSize'] = 2304, -- Don't change
+	['mapWaterColor'] = {0,0,0,0}, -- Adjusted for this map
+	['alpha'] = 240, -- Can change
+};
