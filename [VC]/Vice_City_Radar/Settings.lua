8059381be6b44e659567d2aca428cf5c7698a429
Blue Eagle <bluejaycjl@gmail.com>
2025-04-13 15:07:20 -0400

    Linux fix for Radar
    Linux fix for Radar

--- [VC]/Vice_City_Radar/Settings.lua
+++ [VC]/Vice_City_Radar/Settings.lua
@@ -1,5 +1,5 @@
 radarSettings = {
-	['mapTexture'] = 'images/radar.png', -- Don't change
+	['mapTexture'] = 'Images/radar.png', -- Don't change
 	['mapTextureSize'] = 2304, -- Don't change
 	['mapWaterColor'] = {0,0,0,0}, -- Adjusted for this map
 	['alpha'] = 240, -- Can change
